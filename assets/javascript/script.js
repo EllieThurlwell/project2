171b2ffa58792cab4f7df12bafd1bd9c6032d59e
Ellie Thurlwell <elliethurlwell@yahoo.co.uk>
2021-07-09 14:19:19 +0000

    Add basic function outlines

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -6,7 +6,8 @@ const answers = document.getElementsByClassName('answer-text');// does this need
 const reset = document.getElementById('reset-btn');
 const quit = document.getElementById('quit-btn');
 const previousScore = document.getElementById('prev-score');
-const possAnswers = questionBank;
+
+let possQuestions = questionBank; // how to link between scripts - undefined
 
 let currentQuestion;
 
@@ -48,24 +49,46 @@ for (let button of buttons) {
 }
 
 
+// run game function incorporating loadQuestion etc
 
 
 // in each category need to begin at index 0 and loop through questions, writing to questionText using currentQuestion
 function loadQuestion() {
-    for (let i = 0; i < possAnswers.length; i++) {
-        let currentQuestion = possAnswers[i];
+    for (let i = 0; i < possQuestions.length; i++) {
+        let currentQuestion = possQuestions[i];
         questionText.innerHTML = currentQuestion;
     }
-} // how 
+} // how to pull questions from only one cat not entire bank? function for each cat?
+
 
 // for each question write answers to answers - by array index?
 
-// show current question number in progressCount - index + 1?
+
+// show current question number in progressCount - index + 1 or own variable incremented? own function or in loadQuestion
+function progress() {
+    let questionNo = 1;
+    questionNo ++;
+    progressCount.innerHTML = `${questionNo}`;
+}
+
 
 // if isCorrect=false background color of clicked red and isCorrect green, else isCorrect green
 
-// if isCorrect increment score
 
-//load next question
+// if isCorrect increment score - same qs as progressCount
+
+
+//load next question and call progress()
+
 
 //on completion? display correct answer score? by overwriting html of quiz area?
+
+
+// reset start again at question index 0
+
+
+// quit display alert 'are you sure' then back to home
+
+
+// previousScore using final correct answer amount
+ 
