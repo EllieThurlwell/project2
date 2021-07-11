a727b5944e855207d76f40c465b4325e87be6fb1
Ellie Thurlwell <elliethurlwell@yahoo.co.uk>
2021-07-11 08:24:50 +0000

    Add quiz functions for loading questions and answers

--- assets/javascript/quiz.js
+++ assets/javascript/quiz.js
@@ -1,94 +1,59 @@
-// these are all the elements that will be written to/innerHTML changed. Is const right option for all?
-const questionText = document.getElementById('question-text');
-const correctAnswers = document.getElementById('score');
-const progressCount = document.getElementById('current-q');
-const answers = document.getElementsByClassName('answer-text');// does this need to be made into an array? Array.from()
-const reset = document.getElementById('reset-btn');
-const quit = document.getElementById('quit-btn');
-const previousScore = document.getElementById('prev-score');
 
-let possQuestions = questionBank; // how to link between scripts - undefined
-
-let currentQuestion;
-
-
-
-//control what happens between home page and quiz page on clicking each category choice
-let buttons = document.getElementsByClassName('cat-btn');
 let answerBtns = document.getElementsByClassName('answer-btn');
-console.log(answerBtns);
-
-for (let button of buttons) {
-    button.addEventListener('click', function() {
-         if (this.getAttribute('id') === 'cat1') {
-            //box color. answerBtns.style not working
-            //cat1 function
-            console.log('cat1 clicked');
-        } else if (this.getAttribute('id') === 'cat2') {
-            //box color
-            //cat2 function
-            console.log('cat2 clicked');
-        } else if (this.getAttribute('id') === 'cat3') {
-            //box color
-            //cat3 function
-            console.log('cat3 clicked');
-        } else if (this.getAttribute('id') === 'cat4') {
-            //box color
-            //cat4 function
-            console.log('cat4 clicked');
-        } else if (this.getAttribute('id') === 'cat5') {
-            //box color
-            //cat5 function
-            console.log('cat5 clicked');
-        } else if (this.getAttribute('id') === 'cat6') {
-            //box color
-            //cat6 function
-            console.log('cat6 clicked');
-        } 
+// function initQuiz() {(eachAnswer)
+//   answerBtns.forEach(eachAnswer => { function
+//     eachAnswer.addEventListener('click' , function() {
+//       checkAnswer(this);
+//     ;});
+//   })
+// }
+function initQuiz() {
+    answerBtns.forEach(function (eachAnswer) {
+        eachAnswer.addEventListener('click', function () {
+            checkAnswer(this);
+        });
     });
 }
 
+function getCategoryFromURLParams() {
+  const urlParams = new URLSearchParams(window.location.search);
+  return urlParams.get('categoryId');
+}
 
-// run game function incorporating loadQuestion etc
+function isCategoryValid(categoryId) {
+  const categoriesIds = CATEGORIES.map((eachCategory) => eachCategory.id);
+  return categoriesIds.includes(categoryId);
+}
 
+const currentCategoryId = getCategoryFromURLParams();
+if (!isCategoryValid(currentCategoryId)) {
+//   window.location.href = './';
+} //gets the site stuck in a refresh loop on homepage
 
-// in each category need to begin at index 0 and loop through questions, writing to questionText using currentQuestion
-function loadQuestion() {
-    for (let i = 0; i < possQuestions.length; i++) {
-        let currentQuestion = possQuestions[i];
-        questionText.innerHTML = currentQuestion;
-    }
-} // how to pull questions from only one cat not entire bank? function for each cat?
+const questionsToShow = questionBank.filter(
+  (eachQuestion) => eachQuestion.categoryId === currentCategoryId
+);
 
+let currentQuestionIndex = 0;
+showNextQuestion();
 
-// for each question write answers to answers - by array index?
+function showQuestion(currentQuestionIndex) {
+  const questionDetails = questionsToShow[currentQuestionIndex - 1].question; //.question not working - how else to bring it from bank
+  document.getElementById('question-text').innerText = questionDetails;
+  // Show the question on the UI
+  // In case the option is correct, you need to add a [data-correct=true] when isCorrect = true
+}
 
+function checkAnswer(answerNode) {
+  const isCorrect = answerNode.dataset.correct === 'true';
+ // Increment score;
+  showNextQuestion();
+}
 
-// show current question number in progressCount - index + 1 or own variable incremented? own function or in loadQuestion
-function progress() {
-    let questionNo = 1;
-    questionNo ++;
-    progressCount.innerHTML = `${questionNo}`;
+function showNextQuestion() {
+  currentQuestionIndex++;
+  showQuestion(currentQuestionIndex);
 }
 
 
-// if isCorrect=false background color of clicked red and isCorrect green, else isCorrect green
-
-
-// if isCorrect increment score - same qs as progressCount
-
-
-//load next question and call progress()
-
-
-//on completion? display correct answer score? by overwriting html of quiz area?
-
-
-// reset start again at question index 0
-
-
-// quit display alert 'are you sure' then back to home
-
-
-// previousScore using final correct answer amount
  
